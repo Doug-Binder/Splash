d350266531d93caa87e2ef3ac1dbe9a6f9b97945
Doug-Binder <binderd@stolaf.edu>
2014-10-16 11:06:25 -0400

    streaming tweets from Bind_bot,  awe hell yeah

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ mongoose.connect(uristring)
 
 
 var app = express();
-
+var server = http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port' + app.get('port'));
+})
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +37,22 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
+var Twit = require('twit')
+var T = new Twit({
+	consumer_key:'iJz9VlJ2G3hgKcdffc8HPIf36',
+	consumer_secret:'RfQD9hxeXLxfixb6RVqWoBu1IqMZeJppAvwY9BSlq79wuGSlSe',
+	access_token:'2831637981-ffWoUTteuWRv4S7Tui7z4qcrLLAKfGdbrrGc692',
+	access_token_secret:'el9wOAwI01itqzD0094jw6JByyLz2UZSe7ioTpCDKQotk'
+})
+
+var io = require('socket.io').listen(server);
+var stream = T.stream('statuses/filter', {follow: "2831637981"});
+//io.sockets.on('connection', function(socket){
+	stream.on('tweet', function(tweet){
+		console.log(tweet);
+		//socket.emit('info', {tweet: tweet})
+//	})
+})
 
 var db = mongoose.connection;
 var Article = require('./models/article').Article
@@ -46,6 +64,7 @@ console.log('connection error', err);
 db.once('open', function () {
 console.log('connected.');
 });
+
 
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
